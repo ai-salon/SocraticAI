91b7eb0e219c82e9bbbbbaae7d524bbfa2114235
Ian Eisenberg <ian@credo.ai>
2023-10-10 08:29:47 -0700

    rearranged, added process script

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,6 +1,5 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-from ChatDigest.interpret.interpret import *
+from ChatDigest.interpret.process import process_file
+from ChatDigest.llm_utils import *
 from ChatDigest.prompts.interpret_prompts import (
     archetype_conversation_prompt,
     archetype_template,
@@ -9,7 +8,7 @@ from ChatDigest.prompts.interpret_prompts import (
     summary_template,
     twitter_template,
 )
-from ChatDigest.utils import Prompt
+from ChatDigest.utils import Prompt, chunk_text
 
 # load up the conversation
 file_path = "data/2023-05-25 - Salon: Relationships_transcript.txt"
@@ -17,54 +16,7 @@ file_path = "data/2023-05-25 - Salon: Relationships_transcript.txt"
 with open(file_path, "r") as f:
     text = f.read()
 
-
-p = Prompt(
-    "test",
-    """
-I want you to act as an editor to remove filler content from a conversation transcript. You will be given a long transcript of a multi-hour conversation between two or more people. Your goal is to remove only the fluff - do not summarize or shorten the meaningful content itself. Focus on identifying and eliminating:
-
-Filler words like "um", "uh", "you know", "like", etc.
-Repeated or redundant phrases and dialog
-Verbal tics like "I mean", "basically", "actually", etc.
-Tangents and side conversations that veer off topic
-Excessively long monologues or stories that can be tightened up
-Any dialog that does not move the core conversation forward
-You should trim ONLY fluff - keep all content that contains substantive ideas, insights, questions, or dialog central to the topic. The edited version should retain the full conversation content and all speakers, just tightened up by removing fluff and filler. Do not change the original content itself beyond cleaning up the fluff. Please provide the edited transcript without any summarization.
-
-The conversation is below between two ```:
-
-The conversation is between ```:
-```{conversation}```
-""",
-)
-
-
-def chunk_text(text):
-    text_splitter = RecursiveCharacterTextSplitter(
-        # Set a really small chunk size, just to show.
-        chunk_size=5000,
-        chunk_overlap=0,
-    )
-    docs = text_splitter.create_documents([text])
-    chunks = [d.page_content for d in docs]
-    return chunks
-
-
-text_chunks = chunk_text(text)
-
-processed_chunks = []
-for chunk in text_chunks:
-    processed = chat_completion(p(conversation=chunk), "claude-2", 10000)
-    # remove text at beginning of string
-    to_remove = (
-        "Here is an edited version of the conversation with filler content removed:"
-    )
-    processed = processed.replace(to_remove, "")
-    processed_chunks.append(processed)
-processed_text = "\n".join(processed_chunks)
-output_file = file_path.replace(".txt", "_processed.txt")
-with open(output_file, "w") as f:
-    f.write(processed_text)
+text = process_file(file_path)
 
 
 out = chain_completion(
