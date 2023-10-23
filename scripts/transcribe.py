711065c252c29633393902646a641225bed1f023
Ian Eisenberg <ian@credo.ai>
2023-10-23 15:25:49 -0700

    bug fixes

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -6,13 +6,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("file_path", help="the name of the file to transcribe")
 parser.add_argument(
     "output_file",
-    help="the name of the file to transcribe",
+    help="the name of the transcribed file. Defaults to the same name as the input with a _transcript suffix.",
     default=None,
-    optional=True,
+    nargs="?",
 )
 
 
 args = parser.parse_args()
 
-transcription = transcribe(args.filename, args.output_file)
+transcription = transcribe(args.file_path, args.output_file)
 print(transcription)
