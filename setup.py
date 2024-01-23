85e2e038b2dfc5229ad2c4da59ff99900e1cb1fc
Ian Eisenberg <ianeisenberg90@gmail.com>
2024-01-22 22:16:14 -0800

    updated cli and readme

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "SocraticAI=scripts.cli:main",
+            "SocraticAI=SocraticAI.cli.main:main",
         ]
     },
 )
